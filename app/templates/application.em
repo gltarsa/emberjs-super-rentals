1a241aeb9957e7a28ab1740bfccac36db39e9ccf
Greg Tarsa <gltarsa@gmail.com>
2016-09-28 14:24:44 -0400

    Finishes the tutorial coding

--- app/templates/application.em
+++ app/templates/application.em
@@ -1,12 +1,12 @@
-div.container
-  div.menu
+div class="container"
+  div class="menu"
     = link-to 'index'
-      h1.left
+      h1 class="left"
         <em>SuperRentals</em>
-    div.left.links
+    div class="left links"
       = link-to 'about'
         | About
       = link-to 'contact'
         | Contact
-  div.body
+  div class="body"
     = outlet
--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,26 +1,47 @@
 import { test } from 'qunit';
+import Ember from 'ember';
 import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptance';
 
-moduleForAcceptance('Acceptance | list rentals');
-
-test('should list available rentals.', (assert) => {
-  visit('/');
-  andThen(() => assert.equal(find('.listing').length, 3, 'should see 3 listings'));
+let StubMapsService = Ember.Service.extend({
+  getMapElement() {
+    return document.createElement('div');
+  }
 });
 
-test('should link to information about the company.', (assert) => {
+moduleForAcceptance('Acceptance | list rentals', {
+  beforeEach() {
+    this.application.register('service:stubMaps', StubMapsService);
+    this.application.inject('component:location-map', 'maps', 'service:stubMaps');
+  }
+});
+
+test('should redirect to rentals route', function (assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/rentals', 'should redirect automatically');
+  });
+});
+
+test('should list available rentals.', function (assert) {
+  visit('/');
+  andThen(function () {
+    assert.equal(find('.listing').length, 3, 'should see 3 listings');
+  });
+});
+
+test('should link to information about the company.', function (assert) {
   visit('/');
   click('a:contains("About")');
   andThen(() => assert.equal(currentURL(), '/about', 'should navigate to about'));
 });
 
-test('should link to contact information.', function (assert) => {
+test('should link to contact information.', function (assert) {
   visit('/');
   click('a:contains("Contact")');
   andThen(() => assert.equal(currentURL(), '/contact', 'should navigate to contact'));
 });
 
-test('should filter the list of rentals by city.', (assert) => {
+test('should filter the list of rentals by city.', function (assert) {
   visit('/');
   fillIn('.list-filter input', 'seattle');
   keyEvent('.list-filter input', 'keyup', 69);
@@ -34,7 +55,7 @@ test('should filter the list of rentals by city.', (assert) => {
   });
 });
 
-test('should show details for a specific rental', (assert) => {
+test('should show details for a specific rental', function (assert) {
   visit('/');
   click('a:contains("Grand Old Mansion")');
   andThen(() => {
